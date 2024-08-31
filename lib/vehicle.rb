3f75de9cbb3794198860cb1b280de6ddc412661b
Seth Verrill <verrill.seth@gmail.com>
2024-08-31 14:44:20 -0600

    fix: Clean up for readability

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,8 @@
 require 'date'
 
 class Vehicle
-  attr_reader :vin,
-              :year,
-              :make,
-              :model,
-              :engine
+  attr_reader :vin, :year, :make, :model, :engine
+  attr_accessor :plate_type, :registration_date
               
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -13,17 +10,15 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
+    @registration_date = nil
   end
 
   def antique?
-    Date.today.year - @year > 25
+    Date.today.year - @year >= 25
   end
 
   def electric_vehicle?
     @engine == :ev
   end
-
-  def registration_date
-    nil
-  end
+  
 end
