2bae8f79779beab145d482c78b9ac3732555e924
Seth Verrill <verrill.seth@gmail.com>
2024-09-02 10:08:46 -0600

    test: Add tests for NY facilities. Add specificity to CO tests

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -14,15 +14,16 @@ RSpec.describe FacilityFactory do
     co_data = @dds.co_dmv_office_locations
     facilities = @factory.create_facilities(co_data, 'CO')
 
-    # expect(facilities).to be_an_instance_of(Facility)
+    expect(facilities).to all(be_an_instance_of(Facility))
     expect(facilities.size).to eq(co_data.size)
     
     first_facility = facilities.first
     first_data = co_data.first
 
-    expect(first_facility.name).to eq(first_data[:dmv_office])
-    expect(first_facility.address).to include(first_data[:address_li], first_data[:city], first_data[:state], first_data[:zip])
-    expect(first_facility.phone).to eq(first_data[:phone])
+    expect(first_facility.name).to eq("DMV Tremont Branch")
+    expect(first_facility.address).to eq("2855 Tremont Place, Suite 118, Denver, CO, 80205")
+    expect(first_facility.phone).to eq("(720) 865-4600")
+    expect(first_facility.services).to eq(["vehicle titles", "registration", "renewals;  VIN inspections"])
   
   end
 
@@ -30,15 +31,15 @@ RSpec.describe FacilityFactory do
     ny_data = @dds.ny_dmv_office_locations
     facilities = @factory.create_facilities(ny_data, 'NY')
 
-    # expect(facilities).to be_an_instance_of(Facility)
+    expect(facilities).to all(be_an_instance_of(Facility))
     expect(facilities.size).to eq(ny_data.size)
     
-    first_facility = facilities.first
-    first_data = ny_data.first
+    last_facility = facilities.last
+    last_data = ny_data.last
 
-    expect(first_facility.name).to eq(first_data[:office_name])
-    expect(first_facility.address).to include(first_data[:street_address_line_1], first_data[:city], first_data[:state], first_data[:zip_code])
-    expect(first_facility.phone).to eq(first_data[:public_phone_number])
+    expect(last_facility.name).to eq("MIDTOWN MANHATTAN")
+    expect(last_facility.address).to eq("366 WEST 31ST STREET, NEW YORK, NY, 10001")
+    expect(last_facility.phone).to eq("7189666155")
   end
 
 end
