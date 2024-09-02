5c3b58d2fd9dbb2992dd42a5ee54653b17ef8af6
Seth Verrill <verrill.seth@gmail.com>
2024-09-02 09:29:02 -0600

    feat: Add code to pull data from CO dmv

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -2,4 +2,50 @@ class FacilityFactory
 
   def initialize
   end
+
+  def create_facilities(source, state)
+    facilities = []
+    source.each do |data|
+      facilities << create_facility(data, state)
+    end
+    facilities
+  end
+
+  def create_facility(data, state)
+    case state
+    when 'CO'
+      create_co_facility(data)
+    when 'NY'
+      create_ny_facility(data)
+    when 'MO'
+      create_mo_facility(data)
+    else
+      puts "No data for #{state}"
+    end
+  end
+
+  def create_co_facility(data)
+    address = [
+      data[:address_li],
+      data[:address__1],
+      data[:city],
+      data[:state],
+      data[:zip]
+    ].compact.join(', ')
+
+    facility = Facility.new({
+      name: data[:dmv_office],
+      address: address,
+      phone: data[:phone]
+    })
+
+    services = data[:services_p].split (', ')
+    services.each do |service|
+      facility.add_service(service)
+    end
+    facility
+  end
+
+  
+  
 end
