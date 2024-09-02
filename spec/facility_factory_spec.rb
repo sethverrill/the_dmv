9d8ee805629e77f4befa3640d7896ce725f35940
Seth Verrill <verrill.seth@gmail.com>
2024-09-02 09:45:43 -0600

    test: Add test for NY facilities

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -26,4 +26,19 @@ RSpec.describe FacilityFactory do
   
   end
 
+  it 'can create New York facilities' do
+    ny_data = @dds.ny_dmv_office_locations
+    facilities = @factory.create_facilities(ny_data, 'NY')
+
+    # expect(facilities).to be_an_instance_of(Facility)
+    expect(facilities.size).to eq(ny_data.size)
+    
+    first_facility = facilities.first
+    first_data = ny_data.first
+
+    expect(first_facility.name).to eq(first_data[:office_name])
+    expect(first_facility.address).to include(first_data[:street_address_line_1], first_data[:city], first_data[:state], first_data[:zip_code])
+    expect(first_facility.phone).to eq(first_data[:public_phone_number])
+  end
+
 end
