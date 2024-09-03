d770d0ff2608b60a9c5452197ed48e0ed1c3a70c
Seth Verrill <verrill.seth@gmail.com>
2024-09-03 12:54:26 -0600

    feat: Add code to handle Washington EV data

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -15,4 +15,20 @@ class VehicleFactory
   def create_vehicle(data)
     Vehicle.new(data)
   end
+
+  def create_wa_vehicles(arr)
+    arr.map do |data|
+      create_wa_vehicle(data)
+    end
+  end
+
+  def create_wa_vehicle(data)
+    Vehicle.new({
+      vin: data[:vin_1_10],
+      year: data[:model_year],    
+      make: data[:make],
+      model: data[:model],
+      county: data[:county]
+    })
+  end  
 end
